isislovecruft/python-gnupg

commit 3c925e997eb50a60d4d89fd5772a0fb0f17bc48f

Check that _copy_data() parameters are actually file descriptors.
 * Closes #1920.

gnupg.py
@@ -100,6 +100,13 @@ def _copy_data(instream, outstream):
     """
     sent = 0
 
+    try:
+        assert isinstance(instream, file), "instream is not a file"
+        assert isinstance(outsteam, file), "outstream is not a file"
+    except AssertionError:
+        logger.exception(exc_info=1)
+        return
+
     if hasattr(sys.stdin, 'encoding'):
         enc = sys.stdin.encoding
     else:

commit f06fe7845a1b43f543a1641e3d3bc8448aaec4cd

Fix error introduced in 3c925e997eb50a60d4d89fd5772a0fb0f17bc48f
 * Fix error in _copy_data() due to wrong parameters in call to
   logger.exception. *facepalm*

gnupg.py
@@ -103,8 +103,8 @@ def _copy_data(instream, outstream):
     try:
         assert isinstance(instream, file), "instream is not a file"
         assert isinstance(outsteam, file), "outstream is not a file"
-    except AssertionError:
-        logger.exception(exc_info=1)
+    except AssertionError as ae:
+        logger.exception(ae)
         return
 
     if hasattr(sys.stdin, 'encoding'):
@@ -1074,7 +1074,12 @@ class GPG(object):
         return result
 
     def verify_file(self, file, data_filename=None):
-        "Verify the signature on the contents of the file-like object 'file'"
+        """
+        Verify the signature on the contents of the file-like object 'file'.
+        """
+        ## check that :param:`file` is actually a file:
+        _is_file(file)
+        
         logger.debug('verify_file: %r, %r', file, data_filename)
         result = self.result_map['verify'](self)
         args = ['--verify']