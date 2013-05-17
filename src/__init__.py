81312015ab46046a48610fbd24b087fa903db206
Isis Lovecruft <isis@leap.se>
2013-05-17 02:38:25 +0000

    Remove everything except the GPG class from src/__init__.py.

--- src/__init__.py
+++ src/__init__.py
@@ -6,7 +6,6 @@ from gnupg import GPG
 
 from ._version import get_versions
 __version__ = get_versions()['version']
-del get_versions
 
 gnupg.__version__ = __version__
 gnupg.__author__  = 'Isis Agora Lovecruft'
@@ -14,7 +13,7 @@ gnupg.__contact__ = 'isis@leap.se'
 gnupg.__url__     = 'https://github.com/isislovecruft/python-gnupg'
 gnupg.__license__ = copyleft.disclaimer
 
-__all__ = ["gnupg", "copyright",
-           "Crypt", "DeleteResult", "ListKeys",
-           "GenKey", "Sign", "Encrypt", "ImportResult", "Verify"]
+__all__ = ["GPG"]
+
 del copyleft
+del get_versions
