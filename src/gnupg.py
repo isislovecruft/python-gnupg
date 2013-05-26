c683ef31c274c00484782f045cc37cf0aa3fba4a
Isis Lovecruft <isis@leap.se>
2013-05-26 14:04:33 +0000

    Move old documentation to Sphinx rest file at the end.
     * It seems annoying to have a bunch of licensing and history lessons as the
       first thing you read when you look at the documentation. Rather, the first
       thing you see should be how to install it and use it.

--- src/gnupg.py
+++ src/gnupg.py
@@ -26,51 +26,13 @@ A.M. Kuchling.
 
 This version is patched to sanitize untrusted inputs, due to the necessity of
 executing :class:`subprocess.Popen([...], shell=True)` in order to communicate
-with GnuPG.
+with GnuPG. Several speed improvements were also made based on code profiling,
+and the API has been cleaned up to support an easier, more Pythonic,
+interaction.
 
-:Info: see <https://www.github.com/isislovecruft/python-gnupg>
-:Authors: A.M. Kuchling, Steve Traugott, Vinay Sajip, Isis Lovecruft
-:Date: $Date: 2013-04-04 01:11:01 +0000 (Thursday, April 4, 2013) $
-:Description: Documentation of python-gnupg, a Python module for GnuPG.
-
-
-Previous Authors' Documentation
--------------------------------
-
-Steve Traugott's documentation:
-
-    Portions of this module are derived from A.M. Kuchling's well-designed
-    GPG.py, using Richard Jones' updated version 1.3, which can be found in
-    the pycrypto CVS repository on Sourceforge:
-
-    http://pycrypto.cvs.sourceforge.net/viewvc/pycrypto/gpg/GPG.py
-
-    This module is *not* forward-compatible with amk's; some of the old
-    interface has changed.  For instance, since I've added decrypt
-    functionality, I elected to initialize with a 'gpghome' argument instead
-    of 'keyring', so that gpg can find both the public and secret keyrings.
-    I've also altered some of the returned objects in order for the caller to
-    not have to know as much about the internals of the result classes.
-
-    While the rest of ISconf is released under the GPL, I am releasing this
-    single file under the same terms that A.M. Kuchling used for pycrypto.
-
-    Steve Traugott, stevegt@terraluna.org
-    Thu Jun 23 21:27:20 PDT 2005
-
-
-Vinay Sajip's documentation:
-
-    This version of the module has been modified from Steve Traugott's version
-    (see http://trac.t7a.org/isconf/browser/trunk/lib/python/isconf/GPG.py) by
-    Vinay Sajip to make use of the subprocess module (Steve's version uses
-    os.fork() and so does not work on Windows). Renamed to gnupg.py to avoid
-    confusion with the previous versions.
-
-    A unittest harness (test_gnupg.py) has also been added.
-
-    Modifications Copyright (C) 2008-2012 Vinay Sajip. All rights reserved.
 """
+
+
 
 try:
     from io import StringIO
