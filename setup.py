494592a6ac109886fdf8982923df5f59b4652fdc
Isis Lovecruft <isis@leap.se>
2013-05-28 12:44:52 +0000

    Add headers to setup.py.

--- setup.py
+++ setup.py
@@ -1,5 +1,29 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
+#
+# This file is part of python-gnupg, a Python interface to GnuPG.
+# Copyright © 2013 Isis Lovecruft
+#           © 2008-2012 Vinay Sajip
+#           © 2005 Steve Traugott
+#           © 2004 A.M. Kuchling
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#______________________________________________________________________________
+#
+# NOTE: setuptools is currently (as of 27 May 2013) being merged back into its
+# parent project, distribute. By using the included distribute_setup.py
+# script, we make sure that we have a recent version of setuptools/distribute,
+# which is the *only* Python packaging framework compatible at this point with
+# both Python>=2.4 and Python3.x.
+#
 
 from distutils.core import setup
 
