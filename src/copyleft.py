db7c2760aecf6fced1862f5c242e4cee5299578f
Isis Lovecruft <isis@leap.se>
2013-06-02 13:14:13 +0000

    Add license headers to copyleft.py. INFINITE LICENSE RECURSION! DUN DUN DUN!

--- src/copyleft.py
+++ src/copyleft.py
@@ -1,5 +1,22 @@
-#-*- encoding: utf-8 -*-
-'''
+# -*- coding: utf-8 -*-
+#
+# This file is part of python-gnupg, a Python interface to GnuPG.
+# Copyright © 2013 Isis Lovecruft
+#           © 2008-2012 Vinay Sajip
+#           © 2005 Steve Traugott
+#           © 2004 A.M. Kuchling
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+'''copyleft.py
+--------------
 Copyright information for python-gnupg.
 '''
 
