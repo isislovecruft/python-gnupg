ab00cd69e40c350085e8968dfc066d85c4e76cb3
Isis Lovecruft <isis@leap.se>
2013-06-09 08:11:33 +0000

    Import with from the future.

--- tests/test_gnupg.py
+++ tests/test_gnupg.py
@@ -23,6 +23,7 @@ A test harness and unittests for gnupg.py.
 
 from __future__ import absolute_import
 from __future__ import print_function
+from __future__ import with_statement
 from argparse   import ArgumentParser
 from codecs     import open as open
 from functools  import wraps
