2c0c826c0f55864a39f7c92f4b88b108ebd513fa
drebs <drebs@leap.se>
2013-10-24 18:04:30 -0200

    Fix gpg binary path validation.

--- gnupg/_util.py
+++ gnupg/_util.py
@@ -271,6 +271,8 @@ def _find_binary(binary=None):
             except IndexError as ie:
                 log.info("Could not determine absolute path of binary: '%s'"
                           % binary)
+        elif os.access(binary, os.X_OK):
+            found = binary
     if found is None:
         try: found = _which('gpg')[0]
         except IndexError as ie:
