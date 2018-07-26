d335aa3205ff13e1dd4c6686f1d4c32abb63d86f
Isis Lovecruft <isis@patternsinthevoid.net>
2018-07-26 03:11:29 +0000

    According to Travis, python-2.6 won't work anymore.

--- setup.py
+++ setup.py
@@ -146,7 +146,6 @@ or Linux, with Python 2.6, Python 2.7, Python 3.3, Python 3.4, or PyPy.
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
